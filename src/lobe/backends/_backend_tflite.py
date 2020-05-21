bd01aceb729c1b78e0cb9cc176810cdca214524f
Adi Azulay <azulayadi@gmail.com>
2020-05-21 11:02:56 -0700

    TensorFlow Lite Support (#1)
    * Fixes TF version requirement
    
    * Fixes return in TF backend, shortens output in basic usage
    
    * Adds support for TF Lite
    
    * Update .gitignore
    
    * Removes commented out code from TF backend

--- src/lobe/backends/_backend_tflite.py
+++ src/lobe/backends/_backend_tflite.py
@@ -2,19 +2,69 @@ import numpy as np
 from PIL import Image
 from .._results import PredictionResult
 
-class ImageClassificationModel():
-    __input_key_image = 'Image'
-    __input_key_batch_size = "batch_size"
-    __output_key_labels = 'Labels_idx_000'
-    __output_key_confidences = 'Labels_idx_001'
-    __output_key_prediction = 'Prediction'
+try:
+    import tflite_runtime.interpreter as tflite
 
-    def __init__(self, model_path):
-        self.__model_path = model_path
+except ImportError:
+    # Needs better error text
+    raise ImportError("ERROR: This is a TensorFlow Lite model and requires \
+                      TensorFlow Lite interpreter to be installed on this \
+                      device. Please go to \
+                      https://www.tensorflow.org/lite/guide/python and \
+                      download the appropriate version for you device.")
+
+
+class ImageClassificationModel():
+
+    def __init__(self, signature):
+        self.__model_path = "{}/{}".format(signature.model_path,
+                                           signature.filename)
+        self.__tflite_predict_fn = None
+        self.__labels = signature.classes
+        self.__mean = 127.5
+        self.__std = 127.5
         raise ImportError("TFLite not yet supported")
 
     def __load(self):
-        return None
+        self.__tflite_predict_fn =\
+            tflite.Interpreter(model_path=self.__model_path)
+
+    def __sort_predictions(self, confidences, labels):
+        top_predictions = confidences.argsort()[-5:][::-1]
+        labels = labels
+        sorted_labels = []
+        for i in top_predictions:
+            sorted_labels.append(labels[i])
+        return sorted_labels, confidences[top_predictions]
 
     def predict(self, image: Image.Image) -> PredictionResult:
-        return None
+        if self.__tflite_predict_fn is None:
+            self.__load()
+
+        self.__tflite_predict_fn.allocate_tensors()
+
+        # Add an extra axis onto the numpy array
+        np_image = np.expand_dims(image, axis=0)
+
+        # Converts to floating point and standardize range from -1 to 1.
+        np_image = (np.float32(np_image) - self.__mean) / self.__std
+
+        input_details = self.__tflite_predict_fn.get_input_details()
+        output_details = self.__tflite_predict_fn.get_output_details()
+
+        self.__tflite_predict_fn.set_tensor(input_details[0]['index'],
+                                            np_image)
+        self.__tflite_predict_fn.invoke()
+        top_prediction_output = \
+            self.__tflite_predict_fn.get_tensor(output_details[0]['index'])
+        confidences_output = \
+            self.__tflite_predict_fn.get_tensor(output_details[1]['index'])
+
+        confidences = np.squeeze(confidences_output)
+        top_prediction = top_prediction_output.item().decode('utf-8')
+
+        sorted_labels, top_confidences = self.__sort_predictions(confidences,
+                                                                 self.__labels)
+
+        return PredictionResult(labels=list(zip(sorted_labels,
+                                top_confidences)), prediction=top_prediction)
