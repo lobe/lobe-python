5d4e6b32fb521c21282e65943b5f48ec71777a61
Adi Azulay <adazulay@microsoft.com>
2020-06-15 09:34:04 -0700

    Updates TF version for non armv7l devices

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         "numpy",
         "pillow",
         "requests",
-        "tensorflow>1.14.0,<2;platform_machine!='armv7l'",
+        "tensorflow>=1.15.2,<2;platform_machine!='armv7l'",
         "tensorflow<1.14.0 ; platform_machine=='armv7l'"
     ],
     dependency_links=["https://www.piwheels.org/simple/tensorflow"]
