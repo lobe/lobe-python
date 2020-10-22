bb20f7d77088cb1e2875c3ff4971dc6cc76f8094
Adi Azulay <adazulay@microsoft.com>
2020-10-22 16:29:20 -0700

    advance version to 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="lobe",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=[
